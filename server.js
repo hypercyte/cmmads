6d1a6a610c4f6da54453384bdb29b8cb117e53ef
hypercyte <hypercyte@gmail.com>
2023-03-02 14:55:45 +0000

    server listener

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const http = require('http');
+const port = 3000;
 
 const server = http.createServer((req, res) => {
+    console.log(req);
+});
 
+// Listen for requests at given port.
+// port
+// hostname
+// callback
+server.listen(port, 'localhost', () => {
+    console.log(`Listening for requests on port ${port}`);
 });
