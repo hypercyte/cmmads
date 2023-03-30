5f00793c49a7bdbb0e1a1e44a978a0b2417e9596
hypercyte <hypercyte@gmail.com>
2023-03-30 08:27:01 +0100

    add express to app.js

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+const express = require('express')
 const http = require('http');
 const port = 3000;
+
+// Set up express app
+const app = express();
+
+// Listen for requests
+app.listen(port);
+
+app.get('/', (req, res) => {
+    res.send('<p>CMMADS is running</p>')
+})
 
 const server = http.createServer((req, res) => {
     console.log(req);
