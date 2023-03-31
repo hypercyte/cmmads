9cf475fec3be7843581719d58d58fa3b53387f30
hypercyte <hypercyte@gmail.com>
2023-03-31 16:17:09 +0100

    create pool mysql2

--- services/db.js
+++ services/db.js
@@ -1,2 +1,12 @@
-const mysql = require('mysql2')
-const db_config = require('../config/db_config')
+const mysql = require('mysql2');
+const db_config = require('../config/db_config');
+
+const pool = mysql.createPool({
+    host: db_config.host,
+    user: db_config.user,
+    password: db_config.password,
+    database: db_config.database,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+})
