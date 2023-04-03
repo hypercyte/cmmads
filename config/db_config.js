ab1759080f32a446af0797ac586f499e988c5201
hypercyte <hypercyte@gmail.com>
2023-04-03 12:59:23 +0100

    remove db username option and make env variable

--- config/db_config.js
+++ config/db_config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
     host: 'mujaah.id',
-    user: 'r_mujahid',
+    user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: 'cmmads'
 }
