4a02656021fc2ce61f36bbce70bb0bbdd92bd8d4
hypercyte <hypercyte@gmail.com>
2023-04-04 13:07:37 +0100

    createPrayerTable() function calling model

--- controllers/prayerController.js
+++ controllers/prayerController.js
@@ -6,11 +6,15 @@ const model = require('../models/prayerModel')
 // Do sql check if table exists
 model.checkForPrayerTimes().then(tableExists => {
     console.log("Does prayer time table exist? " + tableExists)
+    createPrayerTimesTable(tableExists);
 }).catch(e => {
     console.log(e)
 });
 
 // Function on what to do after finding out if prayer table exists or not
 // Make function, then call in the .then() block above
-
+function createPrayerTimesTable(alreadyExists) {
+	if (alreadyExists) return;
+	model.createPrayerTable();
+}
 
