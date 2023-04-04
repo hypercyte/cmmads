c4ca053a3ceb787cabdedf64b02742db6e8fd018
hypercyte <hypercyte@gmail.com>
2023-04-04 17:25:33 +0100

    parse csv

--- models/prayerModel.js
+++ models/prayerModel.js
@@ -1,5 +1,36 @@
-const db = require('../services/db')
+const db = require('../services/db');
+const csv = require('csv-parser');
+const fs = require('fs');
 const currentYear = new Date().getFullYear();
+
+// Import prayer times from CSV file provided for the current year
+const importedPrayerTimes = () => {
+    const file = `data/prayer-data-${currentYear}.csv`; // Path to file
+    const results = [] // Initialise results array
+    fs.createReadStream(file)
+        .pipe(csv(  ['DATE',
+                    'DAY',
+                    'FAJR',
+                    'FAJR_JAMAAH',
+                    'SUNRISE',
+                    'DHUHR',
+                    'DHUHR_JAMAAH',
+                    'ASR_1',
+                    'ASR_2',
+                    'ASR_JAMAAH',
+                    'MAGHRIB',
+                    'MAGHRIB_JAMAAH',
+                    'ISHA',
+                    'ISHA_JAMAAH',
+                    'DAY_OF_YEAR'])) // using csv-parser library & setting headers
+        .on('data', (row) => {
+            row['DATE'] = formatDate(row['DATE']); // converting date to ISO standard format
+            results.push(row);
+        })
+        .on('end', () => {
+            return results;
+        })
+}
 
 // Format date to ISO standard format
 // (to allow for use of MYSQL date operators)
@@ -22,9 +53,11 @@ async function checkForPrayerTimes() {
 
 // Create a prayer table for the current year
 async function createPrayerTable() {
+    console.log(`Creating prayer times table for year ${currentYear}...`)
     try {
         const query = `CREATE TABLE prayer_times_${currentYear} (
-            date DATE PRIMARY_KEY
+            date DATE PRIMARY KEY,
+
         )`
     } catch (err) {
         throw err;
