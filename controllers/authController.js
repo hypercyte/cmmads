32073e783ca9a2dca3df1dc6e52c222bbe60f566
hypercyte <hypercyte@gmail.com>
2023-04-14 17:03:30 +0100

    findUser controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,6 +5,11 @@ function insertNewUser(name, username, email, hash) {
     model.insertNewUser(name, username, email, hash)
 }
 
+function findUser(username) {
+    model.findUser(username);
+}
+
 module.exports = {
-    insertNewUser
+    insertNewUser,
+    findUser
 }
