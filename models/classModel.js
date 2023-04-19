fed2d0f5e011399a2997c191e4bf21d1e4ef9073
hypercyte <hypercyte@gmail.com>
2023-04-19 14:14:24 +0100

    class model start

---
+++ models/classModel.js
@@ -0,0 +1,50 @@
+const db = require('../services/db'); // db service
+
+// Enter new user data into the database
+async function insertNewClass(title, desc, active, roomID) {
+    try {
+        const query = `INSERT INTO Classes (
+            \`title\`,
+            \`desc\`,
+            \`active\`,
+            \`roomID\`
+        ) VALUES (
+            '${title}',
+            '${desc}',
+            '${active}',
+            '${roomID}'
+        );`; 
+        const resultset = await db.executeQuery(query); // execute query
+        console.log(resultset);
+    } catch (err) {
+        throw err;
+    }
+}
+
+async function findClassByID(id) {
+    try {
+        const query = `SELECT * FROM Classes WHERE \`ID\` = '${id}'`;
+        const resultset = await db.executeQuery(query);
+        console.log("Found class:");
+        console.log(resultset);
+        return resultset;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function getClasses() {
+    try {
+        const query = `SELECT * FROM Classes`;
+        const resultset = await db.executeQuery(query);
+        return resultset;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+module.exports = {
+    insertNewClass,
+    getClasses,
+    findClassByID
+}
