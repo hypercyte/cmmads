30015e4175f1d21a02acf206b54d71db7512079e
hypercyte <hypercyte@gmail.com>
2023-04-24 12:11:10 +0100

    first message

--- controllers/telegramBotController.js
+++ controllers/telegramBotController.js
@@ -4,3 +4,9 @@ require('dotenv').config();
 const token = process.env.TELEGRAM_TOKEN;
 
 const bot = new telegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+    const id = msg.chat.id;
+    bot.sendMessage(id, "Assalaamu 'Alaykum. You will now receive salah times on a daily basis. If you would like these to stop, type /stop");
+});
+  
