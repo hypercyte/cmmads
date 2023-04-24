bd3eff6b2fe07dd77bde40b01e430bc7d9d4ff19
hypercyte <hypercyte@gmail.com>
2023-04-24 12:21:26 +0100

    subscribers array

--- controllers/telegramBotController.js
+++ controllers/telegramBotController.js
@@ -1,9 +1,12 @@
 const telegramBot = require('node-telegram-bot-api');
+const cron = require('node-cron');
 require('dotenv').config();
 
 const token = process.env.TELEGRAM_TOKEN;
 
 const bot = new telegramBot(token, { polling: true });
+
+const subscribers = [];
 
 bot.onText(/\/start/, (msg) => {
     const id = msg.chat.id;
