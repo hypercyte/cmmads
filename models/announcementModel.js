fd1a24a4ff2f83917ad67406caabe5c4681c05c6
hypercyte <hypercyte@gmail.com>
2023-04-25 12:24:49 +0100

    announce documentation

--- models/announcementModel.js
+++ models/announcementModel.js
@@ -1,6 +1,11 @@
 const db = require('../services/db');
 
 // Get announcements from the database
+
+/**
+ * 
+ * @returns Result set of announcements, pulled from the database.
+ */
 const getAnnouncements = async () => {
     const query = `SELECT * FROM announcements`;
     const resultset = await db.executeQuery(query);
@@ -11,23 +16,31 @@ const getAnnouncements = async () => {
 }
 
 // Edit announcement
+
+/**
+ * 
+ * @param {number} id 
+ * @param {string} newTitle 
+ * @param {string} newContent 
+ */
 const edit = async (id, newTitle, newContent) => {
     const query = `UPDATE announcements SET \`Title\` = ?,\`Content\` = ? WHERE \`ID\` = ${id};`;
     const params = [ newTitle, newContent ];
     const resultset = await db.executeQuery(query, params);
-
-    return resultset;
 }
 
-// Edit announcement
+// Add announcement
+
+/**
+ * 
+ * @param {string} title 
+ * @param {string} content 
+ */
 const add = async (title, content) => {
     const query = `INSERT INTO announcements (\`Title\`,\`Content\`) VALUES (? , ?);`;
     const params = [ title, content ];
     const resultset = await db.executeQuery(query, params);
-
-    return resultset;
 }
-
 
 module.exports = {
     getAnnouncements,
